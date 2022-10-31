49c719d25c32a172de5004aa5f7b124f9157eae9
Oskar <oskarcole@gmail.com>
2022-10-31 15:59:02 -0700

    Update OnlyForwardAuto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OnlyForwardAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OnlyForwardAuto.java
@@ -25,13 +25,6 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.openftc.apriltag.AprilTagDetection;
-import org.openftc.easyopencv.OpenCvCamera;
-import org.openftc.easyopencv.OpenCvCameraFactory;
-import org.openftc.easyopencv.OpenCvCameraRotation;
-
-import java.util.ArrayList;
 
 @Autonomous
 public class OnlyForwardAuto extends LinearOpMode {
