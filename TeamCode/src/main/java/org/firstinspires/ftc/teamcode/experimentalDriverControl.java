3fac0d8a4936c34321c8a4c79d6149b37f538588
AshleyMHaslam <ashley.m.haslam@gmail.com>
2022-12-08 14:53:59 -0800

    Update experimentalDriverControl.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/experimentalDriverControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/experimentalDriverControl.java
@@ -100,6 +100,7 @@ public class experimentalDriverControl extends LinearOpMode {
             double y = -gamepad1.left_stick_y; // Remember, this is reversed!
             double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
             double rx = gamepad1.right_stick_x;
+            //test
 
 
 
